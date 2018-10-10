a92f2f1de9c2e8a815e3a87e5e579bd49fea7eb5
mAbadsa <moh22389@gmail.com>
2018-10-10 12:06:10 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+   $(".overFlow").click(function() {
+       $(".overFlow").removeClass("active");
+       $(this).addClass("active");
+//       $(this).parent().addClass("zoom");
+       
+   });
+//    $(".offersSub").click(function() {
+//       $(".offersSub").removeClass("scale");
+//        $(this).addClass("scale");
+//   });
+    $(".overFlow").click(function() {
+       $(".overFlow").removeClass("active");
+       $(this).addClass("active");
+//       $(this).parent().addClass("zoom");
+       
+   });
+});
